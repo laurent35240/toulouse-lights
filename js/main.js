d93ec7113143f3fbb79ee62556c680dce3b9725a
Laurent Clouet <laurent35240@gmail.com>
2016-10-06 22:41:29 +0200

    Isolate canvasLayer code

--- js/main.js
+++ js/main.js
@@ -33,74 +33,76 @@ var distEastWest = firstMapBounds.getEast() - firstMapBounds.getWest();
 
 var jsonData;
 
-var canvasLayer = L.canvasLayer();
-canvasLayer
-    .delegate(this) // -- if we do not inherit from L.CanvasLayer we can setup a delegate to receive events from L.CanvasLayer
-    .addTo(toulouseMap);
+canvasLayer = function () {
+    this.fetchAndProcessData = function(processFunction, ctx, canvasOverlay, canvasWidth, canvasHeight) {
+        fetch('data/light-coordinates.json')
+            .then(function (response) {
+                return response.json().then(function (json) {
+                    jsonData = json;
+                    processFunction(json, ctx, canvasOverlay, canvasWidth, canvasHeight);
+                })
+            });
+    };
 
-function fetchAndProcessData(processFunction, ctx, canvasOverlay, canvasWidth, canvasHeight) {
-    fetch('data/light-coordinates.json')
-        .then(function (response) {
-            return response.json().then(function (json) {
-                jsonData = json;
-                processFunction(json, ctx, canvasOverlay, canvasWidth, canvasHeight);
-            })
-        });
-}
+    this.drawJsonData = function(json, ctx, canvasOverlay, canvasWidth, canvasHeight) {
+        var zoom = canvasOverlay._map.getZoom();
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+        ctx.fillStyle = "rgba(255,116,0, 0.2)";
+        var ambientLight = .03;
+        var intensity = 1;
+        var radius = Math.max(minRadius, minRadius + (maxRadius - minRadius) * (zoom - minZoom) / (maxZoom - minZoom));
+        var amb = 'rgba(0,0,0,' + (1-ambientLight) + ')';
+        var pixelData;
 
-function drawJsonData(json, ctx, canvasOverlay, canvasWidth, canvasHeight) {
-    var zoom = canvasOverlay._map.getZoom();
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    ctx.fillStyle = "rgba(255,116,0, 0.2)";
-    var ambientLight = .03;
-    var intensity = 1;
-    var radius = Math.max(minRadius, minRadius + (maxRadius - minRadius) * (zoom - minZoom) / (maxZoom - minZoom));
-    var amb = 'rgba(0,0,0,' + (1-ambientLight) + ')';
-    var pixelData;
+        var coord, g, dot, xCoordPixel, yCoordPixel, showLight;
+        for (var i=0; i<json.length; i++) {
+            coord = json[i];
 
-    var coord, g, dot, xCoordPixel, yCoordPixel, showLight;
-    for (var i=0; i<json.length; i++) {
-        coord = json[i];
+            dot = canvasOverlay._map.latLngToContainerPoint(L.latLng(coord[1], coord[0]));
 
-        dot = canvasOverlay._map.latLngToContainerPoint(L.latLng(coord[1], coord[0]));
-
-        if (dot.x >= 0 && dot.x <= canvasWidth && dot.y >= 0 && dot.y <= canvasHeight) {
-            if (displayImgInMap) {
-                // I do not know why this formula but it seems that it's working
-                xCoordPixel = (coord[0] - distEastWest/2 + distNorthSouth/2 - firstMapBounds.getWest()) / distNorthSouth/Math.sqrt(2) * imageCanvas.width + imageCanvas.width/8;
-                yCoordPixel = (coord[1] - firstMapBounds.getSouth()) / distNorthSouth * imageCanvas.height;
-                pixelData = imageCanvas.getContext('2d').getImageData(xCoordPixel,yCoordPixel,1,1).data;
-                showLight = (pixelData[3] == 255);
-            } else {
-                showLight = true;
-            }
-            if (showLight) {
-                g = ctx.createRadialGradient(dot.x, dot.y, 0, dot.x, dot.y, radius);
-                g.addColorStop(1, 'rgba(0,0,0,' + (1-intensity) + ')');
-                g.addColorStop(0, amb);
-                ctx.fillStyle = g;
-                ctx.fillRect(dot.x-radius, dot.y-radius, 2*radius, 2*radius);
+            if (dot.x >= 0 && dot.x <= canvasWidth && dot.y >= 0 && dot.y <= canvasHeight) {
+                if (displayImgInMap) {
+                    // I do not know why this formula but it seems that it's working
+                    xCoordPixel = (coord[0] - distEastWest/2 + distNorthSouth/2 - firstMapBounds.getWest()) / distNorthSouth/Math.sqrt(2) * imageCanvas.width + imageCanvas.width/8;
+                    yCoordPixel = (coord[1] - firstMapBounds.getSouth()) / distNorthSouth * imageCanvas.height;
+                    pixelData = imageCanvas.getContext('2d').getImageData(xCoordPixel,yCoordPixel,1,1).data;
+                    showLight = (pixelData[3] == 255);
+                } else {
+                    showLight = true;
+                }
+                if (showLight) {
+                    g = ctx.createRadialGradient(dot.x, dot.y, 0, dot.x, dot.y, radius);
+                    g.addColorStop(1, 'rgba(0,0,0,' + (1-intensity) + ')');
+                    g.addColorStop(0, amb);
+                    ctx.fillStyle = g;
+                    ctx.fillRect(dot.x-radius, dot.y-radius, 2*radius, 2*radius);
+                }
             }
         }
-    }
 
-    ctx.fillStyle = amb;
-    ctx.globalCompositeOperation = 'xor';
-    ctx.fillRect(0,0,canvasWidth, canvasHeight);
-}
+        ctx.fillStyle = amb;
+        ctx.globalCompositeOperation = 'xor';
+        ctx.fillRect(0,0,canvasWidth, canvasHeight);
+    };
 
-function onDrawLayer(info) {
-    var ctx = info.canvas.getContext('2d');
+    this.onDrawLayer = function(info) {
+        var ctx = info.canvas.getContext('2d');
 
-    if (typeof jsonData == 'undefined') {
-        fetchAndProcessData(drawJsonData, ctx, info.layer, info.canvas.width, info.canvas.height);
-    } else {
-        drawJsonData(jsonData, ctx, info.layer, info.canvas.width, info.canvas.height);
-    }
-}
+        if (typeof jsonData == 'undefined') {
+            this.fetchAndProcessData(this.drawJsonData, ctx, info.layer, info.canvas.width, info.canvas.height);
+        } else {
+            this.drawJsonData(jsonData, ctx, info.layer, info.canvas.width, info.canvas.height);
+        }
+    };
+};
+
+canvasLayer.prototype = new L.CanvasLayer();
+
+var myCanvasLayer = new canvasLayer();
+myCanvasLayer.addTo(toulouseMap);
 
 // Handling clicking on link
 $('#crossLink').click(function () {
     displayImgInMap = !displayImgInMap;
-    canvasLayer.drawLayer();
+    myCanvasLayer.drawLayer();
 });
